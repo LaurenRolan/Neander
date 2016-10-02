449ffcfe7450aa167aab8731730c19f77344a319
cdflion <44637g@gmail.com>
2016-10-02 12:11:22 -0300

    esboço

--- matriz.vhd
+++ matriz.vhd
@@ -32,40 +32,42 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity matriz is
-    Port ( insere : in  STD_LOGIC; -- switch 7
+    Port ( --insere : in  STD_LOGIC; -- switch 7
            clk : in  STD_LOGIC;
-			  rst : in STD_LOGIC; -- switch 6
+			  rst : in STD_LOGIC; -- switch 7
+			  insere: in std_logic; --botao [?]
+			  fim: in std_logic;		--botao [?]
+			  ender: in std_logic_vector(2 downto 0);--switches 6 downto 4
            anodos : out  STD_LOGIC_VECTOR (3 downto 0);
            segmentos : out  STD_LOGIC_VECTOR (6 downto 0);
-           switches : in  STD_LOGIC_VECTOR (3 downto 0); -- switches 0 to 3
+           switches : in  STD_LOGIC_VECTOR (3 downto 0); -- switches 3 downto 0
 			  pc : out STD_LOGIC_VECTOR (7 DOWNTO 0));
 end matriz;
 
 architecture Behavioral of matriz is
 signal rstFSM, clk_x: std_logic := '1';
+signal insere_out,fim_out:std_logic;
 signal ant : std_logic := '0';
 signal web : std_logic_vector (0 downto 0) := "0";
 signal address, data, acc : std_logic_vector (7 downto 0) := "00000000";
 begin
 	process (clk)
-	variable count : integer range 0 to 9 := 0;
 	begin
 		if rst = '1' then
-			count := 0;
 			rstFSM <= '1';
 		elsif rising_edge(clk) then
-			if insere = '1' and ant = '0' then
-				if count <= 8 then count := count + 1; end if;
-				case count is
-					when 1 => web <= "1"; rstFSM <= '1'; address <= "00011110"; data <= "0000" & switches;
-					when 9 => web <= "0"; rstFSM <= '0'; address <= "10000000"; data <= "0000" & switches;
-					when others => web <= "1"; rstFSM <= '1'; address <= address + '1'; data <= "0000" & switches;
-				end case;
-				ant <= insere;
-			else ant <= insere;
-			end if;
+			if(fim_out='1') then
+				rstFSM<='0';
+			elsif(insere_out='1' and rstFSM='1')then
+				rstFSM<='1';
+				data<=data <= "0000" & switches;
+				address <= "00011110"+("000"&ender);
+				web<='1';
+			end if;			
 		end if;
 	end process;
+DEBOUNCER1:entity work.debouncer port map (insere,clk,insere_out);
+DEBOUNCER2:entity work.debouncer port map (fim,clk,fim_out);
 DIVFREQ: entity work.freq_div port map (clk, rst, clk_x);
 NEANDER: entity work.neander2 port map (address, data, web, clk_x, rstFSM, acc, pc);
 VISOR:	entity work.visor port map (acc, clk, anodos, segmentos);
